3688314c13639a734debeed4cfea9bdeebd50373
CosmanAgraz <cosmansy@mcmaster.ca>
2017-03-01 22:29:57 -0500

    whiteCCW function done

--- src/main.c
+++ src/main.c
@@ -1,6 +1,116 @@
 #include<stdio.h>
 #include<stdlib.h>
-//#include"backend.h"
+
+void whiteCCW(int cube[face][facePiece]){
+	
+	//modifies white face pieces
+	int *temp;
+	temp = cube[0][0];
+	cube[0][0] = cube[0][2];
+	cube[0][2] = cube[0][7];
+	cube[0][7] = cube[0][5];
+	cube[0][5] = temp;
+
+	temp = cube[0][1];
+	cube[0][1] = cube[0][4];
+	cube[0][4] = cube[0][6];
+	cube[0][6] = cube[0][3];
+	cube[0][3] = temp;
+
+	//modifies the pieces around the white face
+	temp = cube[1][0];
+	cube[1][0] = cube[5][0];
+	cube[5][0] = cube[4][0];
+	cube[4][0] = cube[2][0];
+	cube[2][0] = temp;
+
+	temp = cube[1][1];
+        cube[1][1] = cube[5][1];
+        cube[5][1] = cube[4][1];
+        cube[4][1] = cube[2][1];
+        cube[2][1] = temp;
+
+	emp = cube[1][2];
+        cube[1][2] = cube[5][2];
+        cube[5][2] = cube[4][2];
+        cube[4][2] = cube[2][2];
+        cube[2][2] = temp;
+}
+
+void greenCCW(int cube[face][facePiece]){
+	int *temp;
+        temp = cube[1][0];
+        cube[1][0] = cube[1][2];
+        cube[1][2] = cube[1][7];
+        cube[1][7] = cube[1][5];
+        cube[1][5] = temp;
+
+        temp = cube[1][1];
+        cube[1][1] = cube[1][4];
+        cube[1][4] = cube[1][6];
+        cube[1][6] = cube[1][3];
+        cube[1][3] = temp;
+}
+
+void redCCW(int cube[face][facePiece]){
+        int *temp;
+        temp = cube[2][0];
+        cube[2][0] = cube[2][2];
+        cube[2][2] = cube[2][7];
+        cube[2][7] = cube[2][5];
+        cube[2][5] = temp;
+
+        temp = cube[2][1];
+        cube[2][1] = cube[2][4];
+        cube[2][4] = cube[2][6];
+        cube[2][6] = cube[2][3];
+        cube[2][3] = temp;
+}
+
+void yellowCCW(int cube[face][facePiece]){
+        int *temp;
+        temp = cube[3][0];
+        cube[3][0] = cube[3][2];
+        cube[3][2] = cube[3][7];
+        cube[3][7] = cube[3][5];
+        cube[3][5] = temp;
+
+        temp = cube[3][1];
+        cube[3][1] = cube[3][4];
+        cube[3][4] = cube[3][6];
+        cube[3][6] = cube[3][3];
+        cube[3][3] = temp;
+}
+
+void blueCCW(int cube[face][facePiece]){
+        int *temp;
+        temp = cube[4][0];
+        cube[4][0] = cube[4][2];
+        cube[4][2] = cube[4][7];
+        cube[4][7] = cube[4][5];
+        cube[4][5] = temp;
+
+        temp = cube[4][1];
+        cube[4][1] = cube[4][4];
+        cube[4][4] = cube[4][6];
+        cube[4][6] = cube[4][3];
+        cube[4][3] = temp;
+}
+
+void orangeCCW(int cube[face][facePiece]){
+        int *temp;
+        temp = cube[5][0];
+        cube[5][0] = cube[5][2];
+        cube[5][2] = cube[5][7];
+        cube[5][7] = cube[5][5];
+        cube[5][5] = temp;
+
+        temp = cube[5][1];
+        cube[5][1] = cube[5][4];
+        cube[5][4] = cube[5][6];
+        cube[5][6] = cube[5][3];
+        cube[5][3] = temp;
+} 
 
 int main(int argc, char* argv[]){
 	//okokokokokokokokokok
